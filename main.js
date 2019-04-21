692137536ee3f6d7396640b2dd1f56d7399c1108
Sarvesh Gupta <sarveshfreakk@gmail.com>
2019-04-21 20:38:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-{
-  tis:'1'
-}
+// Twilio Credentials
+const accountSid = 'ACdc86b4aa9724e7637dc48e4be51f7589';
+const authToken = '5235dbf054732664f73fc3f2e499a34a';
+
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create(
+  {
+    to: '+918299689170',
+    from: '+12063396774',
+    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+  },
+  (err, message) => {
+    if(err){
+      //console.log(err);
+      return {"err": err}
+    } else {
+      //console.log(message.sid);
+      return {"sid": message.sid}
+    }
+  }
+);
